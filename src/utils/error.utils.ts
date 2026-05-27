8453e1631a4ad8dc0fb846a646980c2251f19d4e
VTL-Repo <vuthienloct@gmail.com>
2026-05-27 22:31:10 +0700

    Initialize backend with basic structure

--- src/utils/error.utils.ts
+++
@@ -1,9 +0,0 @@
-import type { Response } from "express";
-
-export const errorHandler = (res: Response, error: any) => {
-  console.error("Error details:", error);
-  res.status(500).json({
-    message: "Server Error!",
-    error: error.message,
-  });
-};
